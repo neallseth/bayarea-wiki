6e72ee38c918ab35a30b9d4328b3e3ebef27f9d0
neallseth <32910906+neallseth@users.noreply.github.com>
2024-11-25 16:55:52 -0800

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { geistSans, geistMono } from "./fonts/fonts";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Bay Area Wiki",
@@ -18,6 +19,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-dvh p-6 sm:p-8 flex flex-col max-w-[70ch] mx-auto`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
